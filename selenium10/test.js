9295eab7a536feafd9cd451d417433fa1e663554
Szabó Liza <szbliza97@gmail.com>
2024-03-08 19:26:27 +0100

    Class-finder tests

--- selenium10/test.js
+++ selenium10/test.js
@@ -11,29 +11,29 @@ async function runTest() {
     try{
 
         const filePath = path.join(__dirname, 'config.json')
-        const { productName, url } = await fs.readFile(filePath, 'utf-8').then(JSON.parse)
+        const { password, url, productName } = await fs.readFile(filePath, 'utf-8').then(JSON.parse)
 
         await driver.get(url)
-        const searchBox = await driver.findElement(By.xpath('//*[@id="gh-ac"]'))
+        const searchBox = await driver.findElement(By.xpath('//*[@id="password"]'))
 
-        await searchBox.sendKeys(productName, Key.RETURN)
+        await searchBox.sendKeys(password, Key.RETURN)
 
-        const searchResults = await driver.findElements(By.className('s-item'))
+       
+
+        // const check = await driver.findElement(By.xpath('//input[@name="submitted"]'))
+        // await driver.wait(5000)
+        // await check.click()
+
+        // const next = await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/button[2]'))
+        // await next.click()
+        // const understood = await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/button[2]'))
+        // await understood.click()
+        
+        const searchResults = await driver.findElements(By.className('MuiPaper-root'))
+
 
         console.log(`${productName} találatok száma / oldal : ${searchResults.length}`) 
 
-    
-
-        const searchResultFilter = searchResults.filter ( async (el) => {
-            const attributeValue = await el.getAttribute('data-viewport')
-            console.log(attributeValue)
-            if(attributeValue){
-            
-                
-                return el}
-        })
-
-        console.log(searchResultFilter.length)
 
         let idCounter = 1
 
@@ -41,13 +41,13 @@ async function runTest() {
             id: idCounter,
             productName,
             url,
-            searchResults: searchResultFilter.length,
+            searchResults: searchResults.length,
             key: productName
         }
 
         await fs.writeFile('result.json', JSON.stringify(dataToWrite, null, 2))
 
-        if (searchResultFilter.length < 65){
+        if (searchResults.length < 65){
             hibajegyId = 0
             const errorMessage = ` Hibajegy id: ${hibajegyId} tesztId: ${idCounter} product: ${productName} datum: ${new Date().toISOString()} Tesztelo: Liza `
 
